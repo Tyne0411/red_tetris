1f881c4d92ea6d84d146450bbb4a5a60fbfbdfe5
Tyne0411 <TeeEe0411@outlook.com>
2022-06-15 00:14:30 +0200

    test

--- frontend/tests/test.js
+++ frontend/tests/test.js
@@ -4,3 +4,19 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
 });
+
+test('index page has button', async ({ page }) => {
+	await page.goto('/');
+
+	let button = page.locator('button').first()
+	await expect(button).toBeVisible();
+	await expect(button).toHaveText('PLAY');
+})
+
+test('login', async ({ page }) => {
+	await page.goto('/');
+
+	await page.fill('input', 'myname');
+	await page.click('button');
+	await page.screenshot({path: 'screenshot.png'});
+})
