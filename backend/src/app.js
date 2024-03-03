4472ed71b7e9169a06386c9bd4f961e5c6dc865e
Tyne0411 <TeeEe0411@outlook.com>
2024-03-03 07:59:54 -0800

    conflict

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,27 @@
 import { Server } from "socket.io";
 import { Game } from './Game.js';
 import { connectMongo, scoresDB } from './mongodb.js';
-import { Client } from './Client.js'
+import { Client } from './Client.js';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 
-export const io = new Server({
+let rooms = new Map();
+
+const server = process.env.HTTPS
+	? https.createServer({
+		key: fs.readFileSync(process.env.HTTPS_PRIVKEY),
+		cert: fs.readFileSync(process.env.HTTPS_CERT)
+	})
+	: http.createServer();
+
+const PORT = 4000;
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+export const io = new Server(server, {
 	cors: {
 		origin: "*",
-		methods: ["GET", "POST"]
+		methods: ["GET"]
 	}
 });
 
@@ -140,7 +155,3 @@ function start() {
 	
 	});
 }
-
-connectMongo().then(start)
-
-io.listen(4000);
